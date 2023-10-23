8b20e7c9c4f576d9b2d4b7a91a99fe2de88f6a03
habibGrande <snomo22dy@gmail.com>
2023-10-23 21:14:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+
+var sum=9;
+var sumELE = document.querySelector("#sum");
+console.log(sumELE)
+function add9(){
+    sum++;
+    sumELE.innerText= sum + " Like(s)";
+    console.log(sum);
+}
+var num=12;
+var numELE = document.querySelector("#num");
+console.log(numELE)
+function add12(){
+    num++;
+    numELE.innerText= num + " Like(s)";
+    console.log(num);
+}
+var rum=9;
+var rumELE = document.querySelector("#rum");
+console.log(sumELE)
+function addto9(){
+    rum++;
+    rumELE.innerText= rum + " Like(s)";
+    console.log(rum);
+}
+
+var count=3;
+var countElement = document.querySelector("#count");
+console.log(countElement)
+function add1(){
+    count++;
+    countElement.innerText = count +" Like(s)" ;
+    console.log(count);
+}
